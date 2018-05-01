ef51234798b62a0de7a4c1e203bd30fe581dcd26
2017-08-23 <sschofie@janus15.cs.trinity.edu>
2018-05-01 10:42:13 -0500

    05/01

--- src/main/scala/basics/Sorts.scala
+++ src/main/scala/basics/Sorts.scala
@@ -39,14 +39,40 @@ object Sorts {
   }
   //good version, see book for better
   def quickSort[A](arr: Array[A])(lt: (A, A) => Boolean): Unit = {
+    def insertionHelper(arr: Array[A], start: Int, end: Int)(lt: (A, A) => Boolean): Unit = {
+      for(i <- start+1 until end){
+        var j = i
+        var tmp = arr(j)
+        while(j > 0 && lt(tmp, arr(j-1))){
+          arr(j) = arr(j-1)
+          j -= 1
+        }
+        arr(j) = tmp
+      }
+    }
     def helper(start: Int, end: Int): Unit = {
-      if(end - start < 10) insertionHelper(arr, start, end) else {
+      if(end - start < 10) insertionHelper(arr, start, end)(lt) else {
         val pIndex = util.Random.nextInt(end-start)+start
-        val temp = arr(start)
+        val tmp = arr(start)
         arr(start) = arr(pIndex)
-        arr(pIndex) = temp
+        arr(pIndex) = tmp
         var low = start + 1 
-        var hight = end -1
+        var high = end -1
+        while(low <= high){
+          if(lt(arr(low), arr(start))){
+            low += 1
+          }else{
+            val tmp = arr(low)
+            arr(low) = arr(high)
+            arr(high) = tmp
+            high -= 1
+          }
+        }
+        val tmp2 = arr(high)
+        arr(high) = arr(start)
+        arr(start) = tmp2
+        helper(start, high)
+        helper(low, end)
       }
     }
     helper(0, arr.length)
@@ -54,10 +80,10 @@ object Sorts {
   
   
   def main(args: Array[String]): Unit = {
-    val nums = Array.fill(10)(util.Random.nextInt(100))
-    println(nums.mkString(", "))
+    val nums = Array.fill(1000000)(util.Random.nextInt(100))
+    println(nums.take(20).mkString(", "))
     bubbleSort(nums)
-    println(nums.mkString(", "))
+    println(nums.take(20).mkString(", "))
     
     val nums2 = Array.fill(10)(math.random()) 
     //Array is Double so Function must be made POLYMORPHIC to accept all types using <%
@@ -67,6 +93,10 @@ object Sorts {
     
     bubbleSort2(nums)(_%10 > _%10)
     
+    val bigArray = Array.fill(10)(util.Random.nextInt(100))
+    println(bigArray.mkString(" "))
+    quickSort(bigArray)(_<_)
+    println(bigArray.mkString(" "))
   }
   
 }
